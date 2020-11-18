09e7c953442176cd862e4456776ecdbf5c506bd1
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2020-11-18 17:08:58 +0600

    Some class-43 Update

---
+++ Class-43/7. PHP Pre-built Functions/1_math_functions.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo pow(2,7);
+
+echo "<br>";
+
+echo rand(1, 6);
+
+echo "<br>";
+
+echo sqrt(100);
+
+echo "<br>";
+
+echo ceil(4.6);
+
+echo "<br>";
+
+echo floor(4.6);
+
+echo "<br>";
+
+echo round(4.5);
+
---
+++ Class-43/7. PHP Pre-built Functions/2_array_functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+$list = [343,34,323,23,54,232,453];
+
+echo max($list);
+
+echo "<br>";
+    
+echo min($list);
+
+
+echo "<br>";
+
+sort($list);
+    
+print_r($list);
+
+echo in_array(453, $list); // If true then prints 1 or prints nothing.
+
+
